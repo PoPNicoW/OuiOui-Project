086c0d97410caffd8830bc5ecba8940452cb4220
PoPNicoW <n.bouige@dimsi.fr>
2018-10-09 18:29:43 +0200

    creation nginx

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,8 @@ provider "aws" {}
 resource "aws_instance" "nginx01" {
   ami = "ami-075b44448d2276521"
   instance_type = "t2.micro"
-  vpc_security_ids = ["${aws_security_group.nginx01_SEC.id}"]
+  key_name = "NicoKey"
+  vpc_security_group_ids = ["${aws_security_group.nginx01_SEC.id}"]
 
 tags { 
   Name = "nginx01"
@@ -21,7 +22,16 @@ resource "aws_security_group" "nginx01_SEC" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     }
+
+  ingress {
+     from_port = 22
+     to_port = 22
+     protocol =  "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 }
 
-
+output "public_ip_nginx01" {
+  value = "${aws_instance.nginx01.public_ip}"
+}
   
